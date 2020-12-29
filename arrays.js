e78cceca73e397d587c143ee4eb018b64e92d864
Parker Strand <parker.strand@verizon.net>
2020-12-29 18:26:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, item){
   ["item", ...array]
-  return results;
+  return array;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, item){
