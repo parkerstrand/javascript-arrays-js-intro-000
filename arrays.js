37b49757c1682eb79f087a255b7be0835d09b4ab
Parker Strand <parker.strand@verizon.net>
2020-12-29 17:52:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, item){
   return newArray;
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
+function destructivelyAddElementToBeginningOfArray(array, item){
   array.unshift(item);
   return array;
 }
